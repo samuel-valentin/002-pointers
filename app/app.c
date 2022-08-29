3b43258d61a86962c337f2e8141bbb6fe18923f8
Samuel Valentin <samuelv.lopez@iteso.mx>
2022-08-28 22:47:55 -0500

    Pointers apuntando a arreglos que son ordenandos por diferentes metodos Declaracion de metodos

--- app/app.c
+++ app/app.c
@@ -1,4 +1,12 @@
+#include "pointers.h"
+
 int main(void)
 {
+    int array [] = {1,6,3,8,2,3,3,4,1,2,4,1,1,1};
+    int size = sizeof(array) / sizeof(array[0]);
+    bubble_sort(array,size);
+    insertion_sort(array, size);
+    reverse(array,size);
+    calulate_frequency(array, size,1);
   return 0;
 }
--- lib/pointers.c
+++ lib/pointers.c
@@ -5,23 +5,65 @@
 
 #include "pointers.h"
 
-int bubble_sort(int *target, int count)
+int bubble_sort(int *collection, int count)
 {
-  // aqui su implementación.
-  return 0;
+    printf("Bubble Sort\n");
+    int aux;
+    int changes =0;
+    for (int current = 0; current < count - 1; ++current) {
+        for (int next = 0; next < count - current - 1; ++next) {
+            if (collection[next] > collection[next + 1]){
+                aux = collection[next];
+                collection[next] = collection[next + 1];
+                collection[next + 1] = aux;
+                ++changes;
+            }
+        }
+    }
+    for (int i = 0; i <count ; ++i) {
+        printf(" [%d]",collection[i]);
+    }
+    printf("\nCambios necesarios: %d",changes);
+  return changes;
 }
 
 void insertion_sort(int *collection, int count)
 {
-
+    printf("\nInsertion Sort\n");
+    int cambios =0;
+    for (int i = 1; i<count; i++){
+        int key = collection[i];
+        int aux = i -1;
+        while (key < collection[aux] && aux >= 0){
+            collection[aux + 1] = collection[aux];
+            aux--;
+            cambios ++;
+        }
+        collection[aux + 1] = key;
+    }
+    for (int i = 0; i <count ; ++i) {
+        printf("[%d] ",collection[i]);
+    }
+    printf("\nCambios necesarios: %d",cambios);
 }
 
 void reverse(int *collection, int count)
 {
-
+    printf("\nReverse\n");
+    for (int i = count -1; i > 0 ; --i) {
+        printf("[%d] ",*(collection+i));
+    }
 }
 
 int calulate_frequency(int *collection, int count, int target)
 {
-  return 0;
+    printf("\nCalculate fecuency\n");
+    int frequency = 0;
+    for (int i = 0; i < count; i++){
+        if (*(collection+i)==target){
+            frequency++;
+        }
+    }
+    printf("%d",frequency);
+  return frequency;
 }
